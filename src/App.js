4c76586bdf6bf8bdedca499af9d1cfa9644d225f
Tushar Gopale <gopaletushar5@gmail.com>
2025-03-07 13:33:09 +0530

    just for fun!

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+        <Navbar title="Tushar" aboutText="About Tushar" />
+    </>
+  );
+}
+
+export default App;
+ 
